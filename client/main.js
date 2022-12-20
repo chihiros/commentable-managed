26aca0a1460f7d8fcf62a81714eb5f5521d00c77
chihiros <szrkw2ui2a@gmail.com>
2022-12-21 06:16:04 +0900

    fix: hostnameを戻す

--- client/main.js
+++ client/main.js
@@ -109,8 +109,8 @@ app.whenReady().then(() => {
     if (is_windows) tray = new Tray(`${__dirname}/images/icon.ico`);
     else if (is_mac) tray = new Tray(`${__dirname}/images/icon.png`);
 
-    // const hostname = "https://commentable.fly.dev";
-    const hostname = "http://localhost:8080";
+    const hostname = "https://commentable.fly.dev";
+    // const hostname = "http://localhost:8080";
     // const hostname = proccess.env.HOSTNAME;
 
     let contextMenu = Menu.buildFromTemplate([
